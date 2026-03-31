2822ba9bdaaec9b765cd61c235acb5be9b7a0a71
shivmSingh1 <shivams7380@gmai.com>
2026-03-31 17:22:37 +0530

    chnages

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.set("io", io) //set socket server in app
 
 app.use(cookieParser());
 app.use(cors({
-	origin: "https://fooddelivery-beta.vercel.app",
+	origin: "fooddelivery-mocha.vercel.app",
 	credentials: true
 }))
 app.use(express.json())
