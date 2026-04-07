78c21372a1f0d71a973843942abd4588b5c38352
shivmSingh1 <shivams7380@gmai.com>
2026-04-08 02:26:11 +0530

    use effect is not defined

--- frontend/src/pages/CompleteProfile.jsx
+++ frontend/src/pages/CompleteProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { serverUrl } from "../App";
 import { useNavigate } from "react-router-dom";
